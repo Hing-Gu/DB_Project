a48ad14aad7ef6b9274a0c62cadca38e1cf74df3
Sorrel <43405887+Hing-Gu@users.noreply.github.com>
2021-06-05 03:29:07 +0900

    join and view

---
+++ WebContent/view.sql
@@ -0,0 +1,6 @@
+create or replace view dbstudent (s_id, s_major, e_c_id)
+as
+select s.s_id, s.s_major, e.e_c_id
+from students s, enroll e
+where s.s_id = e.e_s_id and e_c_id = 'M300'
+with read only;
